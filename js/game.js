05868561f4471425ff70584de9f4301a86151a44
tygamer1to7 <11811416+tygamer1to7@users.noreply.github.com>
2019-03-06 14:06:35 -0600

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,23 @@
+function mainGame(){
+document.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "ArrowLeft") {
+        //insert code later
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+function gameMoveEntity(x,y){
+
+}
